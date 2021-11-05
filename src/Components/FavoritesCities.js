de104b20364a2f98d3a9020309f6621e3c552a49
dianeyasminec <dianeyasminec60@gmail.com>
2021-11-05 13:49:28 -0400

    completed

--- src/Components/FavoritesCities.js
+++ src/Components/FavoritesCities.js
@@ -1,83 +1,80 @@
-import React ,{ useState, useEffect }from 'react';
-
+import React,{useState, useEffect} from 'react';
+import data from './data/db.json';
 
 function FavoritesCities(){
+const [city, setCity] = useState("")
+const [favorites, setFavorites] = useState([])
 
-    const [favorite,setFavorite] = useState("All")
-    const [cities,setCities] = useState([])
-   // const [fav_cities,setFav_cities] =useState([])
-    let fav_cities
-    const handleChange = (event)=> {
-        setFavorite({
-          ...favorite,city:event.target.value });
-      }
-
-    const handleOnSubmit= (event) =>{
-      event.preventDefault()
-      
-      
-      
-        
-        const newcity={id:"",title:favorite.city}
-        if(favorite.city==null || favorite.city==''){
-          console.log("INPUT VALUE:"+favorite.city)
-          alert('Please enter a city')
-          return
-        }
-        //debugger
-        fetch(`http://localhost:3000/favoritecities`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newcity),
-          })
-          .then(alert("City has been added to your favorite cities!"))
-          window.location.reload();
-          }
-          useEffect(() =>{
- 
-            fetch(`http://localhost:3000/favoritecities`)
-               .then(response => response.json())
-               .then ((data) => {
-                   console.log(data)
-                   //let rep={...data}
-                  //  console.log("DATA FROM FETCH:"+JSON.stringify(data))
-                   setCities(cities=>[...cities,data] )
-                 
-               })       
-         
-              
-           },[setCities])
-          //  console.log("CHECKING CITIES:"+JSON.stringify(cities))
-           if(cities[0]!==undefined){
-               fav_cities=cities[0].map((city) => {
-              return <tr  key={city.id}><td>{city.title}</td> </tr>
-                   
-            })
-            //setFav_cities(fav_cities=>[...fav_cities,f_cities])
-
-           }
-          
-
-return(
-    <div>
-        <form>
-            <input onChange={handleChange} type="text" placeholder=" city" value={favorite.city} name="city"></input>
-            <button onClick={handleOnSubmit} >Save</button>
-        </form>
-         <table border='2'>
-           <tr>
-             <th>City Name</th>
-           </tr>
-           {fav_cities}
-           </table>
-          
-         
-         
-        
-         
-    </div>
-    )
+const addToFavorites = data => {
+  setFavorites(favorites => [...favorites, data]);
 }
-export default FavoritesCities;
+
+//**************************** */
+const handleOnChange = (event) =>{
+  setCity(event.target.value)
+}
+
+const getData = () => {
+  fetch(`http://localhost:3000/favoritecities`,{
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+  .then(response => response.json())
+  .then(data => { 
+  const filteredCities = data.map(city => city.title ? city.title : '');
+  setFavorites(filteredCities);      
+   })
+}
+
+useEffect(() => {
+  getData();
+}, [])
+
+const handleSubmit= (event) => {
+  event.preventDefault();
+  
+if(city !==null || city !==''){
+  console.log('attempting...');
+  fetch(`http://localhost:3000/favoritecities`,{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: city
+    })
+  })
+  .then(res => res.json())
+  .then(() => { 
+    console.log('success!!');
+    getData();
+   })
+  }
+}
+
+//******************************************************* */
+  const showFavorites = () => {
+    return (
+      <>
+        <ul>
+          {favorites.map(favorite => favorite ? <li>{favorite}</li> : '')}
+        </ul>
+      </>
+    )
+  }
+  return(
+    <div>
+<h1> Welcome to Favorites cities </h1>
+<form onSubmit={handleSubmit}>
+  <input type="text" placeholder="Enter your city" value = {city} onChange={e=>handleOnChange(e)}></input>
+  <button>SAVE CITY</button>
+</form>
+<hr/>
+<h2> Here are my favorites: </h2>
+<span>{showFavorites()}</span>
+    </div>
+  )}
+
+export default FavoritesCities
